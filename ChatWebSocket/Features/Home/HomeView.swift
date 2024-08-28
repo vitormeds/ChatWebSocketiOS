791d8bf8f8f95b25bfe9374b3da8612e2d549910
Vitor Mendes <vitor.mendes.souza@outlook.com>
2024-08-27 23:01:54 -0300

    Automatic scroll when send message

--- ChatWebSocket/Features/Home/HomeView.swift
+++ ChatWebSocket/Features/Home/HomeView.swift
@@ -1,40 +1,47 @@
-//
-//  HomeView.swift
-//  ChatWebSocket
-//
-//  Created by Vitor Mendes on 25/08/24.
-//
-
 import SwiftUI
 
 struct HomeView: View {
-    
     @StateObject private var viewModel = HomeViewModel()
 
     var body: some View {
-        List(self.viewModel.messages) { message in
-            ChatMessageView(message: message, isSentByCurrentUser: message.user == viewModel.user)
-                .listRowSeparator(.hidden)
-        }.onAppear(perform: {
-            viewModel.conect()
-        })
-        .listStyle(.plain)
-        .padding(.top, 8)
-        Spacer()
-        HStack {
-            TextField("Digite uma mensagem...", text: $viewModel.newMessage)
-                .onSubmit {
-                sendMessage()
+        VStack {
+            ScrollViewReader { scrollViewProxy in
+                List(self.viewModel.messages) { message in
+                    ChatMessageView(message: message, isSentByCurrentUser: message.user == viewModel.user)
+                        .listRowSeparator(.hidden)
+                        .id(message.id)
+                }
+                .onAppear(perform: {
+                    viewModel.conect()
+                })
+                .listStyle(.plain)
+                .padding(.top, 8)
+                .onChange(of: viewModel.messages, {
+                    if let lastMessage = viewModel.messages.last {
+                        withAnimation {
+                            scrollViewProxy.scrollTo(lastMessage.id, anchor: .bottom)
+                        }
+                    }
+                })
             }
-            Button {
-                sendMessage()
-            } label: {
-                Text("Enviar")
+
+            Spacer()
+
+            HStack {
+                TextField("Digite uma mensagem...", text: $viewModel.newMessage)
+                    .onSubmit {
+                        sendMessage()
+                    }
+                Button {
+                    sendMessage()
+                } label: {
+                    Text("Enviar")
+                }
             }
+            .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
         }
-        .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
     }
-    
+
     func sendMessage() {
         viewModel.sendMessage()
         viewModel.newMessage = ""
